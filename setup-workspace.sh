61314b31384a3399a3504b85d21b9c3d1fe8175a
Sean Sundberg <seansund@us.ibm.com>
2022-07-15 13:17:05 -0500

    Regens setup-workspace
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- setup-workspace.sh
+++ setup-workspace.sh
@@ -133,6 +133,7 @@ cat "${SCRIPT_DIR}/terraform.tfvars.template-${FLAVOR,,}" | \
 if [[ ! -f "${WORKSPACE_DIR}/gitops.tfvars" ]]; then
   cat "${SCRIPT_DIR}/terraform.tfvars.template-gitops" | \
     sed -E "s/#(.*=\"GIT_HOST\")/${GITHOST_COMMENT}\1/g" | \
+    sed "s/PREFIX/${PREFIX_NAME}/g"  | \
     sed "s/GIT_HOST/${GIT_HOST}/g" \
     > "${WORKSPACE_DIR}/gitops.tfvars"
 fi
