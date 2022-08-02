c2fffb584e438d7fb2f932159ef6e2d7407202ab
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-08-03 06:23:57 +1000

    Updated gitops module for gitea
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- 2-standard/200-ibm-openshift-gitops/main.tf
+++ 2-standard/200-ibm-openshift-gitops/main.tf
@@ -16,32 +16,18 @@ module "argocd-bootstrap" {
   sealed_secret_private_key = module.sealed-secret-cert.private_key
 }
 module "cluster" {
-  source = "cloud-native-toolkit/ocp-vpc/ibm"
-  version = "1.15.6"
+  source = "github.com/cloud-native-toolkit/terraform-ocp-login?ref=v1.4.2"
 
-  cos_id = var.cluster_cos_id
-  disable_public_endpoint = var.cluster_disable_public_endpoint
-  exists = var.cluster_exists
-  flavor = var.cluster_flavor
-  force_delete_storage = var.cluster_force_delete_storage
-  ibmcloud_api_key = var.ibmcloud_api_key
-  kms_enabled = var.cluster_kms_enabled
-  kms_id = var.cluster_kms_id
-  kms_key_id = var.cluster_kms_key_id
-  kms_private_endpoint = var.cluster_kms_private_endpoint
-  login = var.cluster_login
-  name = var.cluster_name
-  name_prefix = var.name_prefix
-  ocp_entitlement = var.cluster_ocp_entitlement
-  ocp_version = var.ocp_version
-  region = var.region
-  resource_group_name = module.resource_group.name
-  sync = module.resource_group.sync
-  tags = var.cluster_tags == null ? null : jsondecode(var.cluster_tags)
-  vpc_name = var.cluster_vpc_name
-  vpc_subnet_count = var.cluster_vpc_subnet_count
-  vpc_subnets = var.cluster_vpc_subnets == null ? null : jsondecode(var.cluster_vpc_subnets)
-  worker_count = var.worker_count
+  ca_cert = var.cluster_ca_cert
+  ca_cert_file = var.cluster_ca_cert_file
+  cluster_version = var.cluster_cluster_version
+  ingress_subdomain = var.cluster_ingress_subdomain
+  login_password = var.cluster_login_password
+  login_token = var.cluster_login_token
+  login_user = var.cluster_login_user
+  server_url = var.server_url
+  skip = var.cluster_skip
+  tls_secret_name = var.cluster_tls_secret_name
 }
 module "config" {
   source = "github.com/cloud-native-toolkit/terraform-gitops-cluster-config?ref=v1.0.0"
@@ -54,15 +40,45 @@ module "config" {
   namespace = module.toolkit_namespace.name
   server_name = module.gitops_repo.server_name
 }
+module "gitea" {
+  source = "github.com/cloud-native-toolkit/terraform-tools-gitea?ref=v0.4.1"
+
+  cluster_config_file = module.cluster.config_file_path
+  cluster_type = module.cluster.platform.type_code
+  instance_name = var.gitea_instance_name
+  instance_namespace = module.gitea_namespace.name
+  olm_namespace = module.olm.olm_namespace
+  operator_namespace = module.olm.target_namespace
+  password = var.gitea_password
+  username = var.gitea_username
+}
+module "gitea_namespace" {
+  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace?ref=v3.2.3"
+
+  cluster_config_file_path = module.cluster.config_file_path
+  create_operator_group = var.gitea_namespace_create_operator_group
+  name = var.gitea_namespace_name
+}
+module "gitea_namespace1" {
+  source = "github.com/cloud-native-toolkit/terraform-gitops-namespace?ref=v1.11.2"
+
+  argocd_namespace = var.gitea_namespace1_argocd_namespace
+  ci = var.gitea_namespace1_ci
+  create_operator_group = var.gitea_namespace1_create_operator_group
+  git_credentials = module.gitops_repo.git_credentials
+  gitops_config = module.gitops_repo.gitops_config
+  name = var.gitea_namespace1_name
+  server_name = module.gitops_repo.server_name
+}
 module "gitops_repo" {
   source = "github.com/cloud-native-toolkit/terraform-tools-gitops?ref=v1.21.0"
 
   branch = var.gitops_repo_branch
   debug = var.debug
-  gitea_host = var.gitops_repo_gitea_host
-  gitea_org = var.gitops_repo_gitea_org
-  gitea_token = var.gitops_repo_gitea_token
-  gitea_username = var.gitops_repo_gitea_username
+  gitea_host = module.gitea.host
+  gitea_org = module.gitea.org
+  gitea_token = module.gitea.token
+  gitea_username = module.gitea.username
   gitops_namespace = var.gitops_repo_gitops_namespace
   host = var.gitops_repo_host
   org = var.gitops_repo_org
@@ -93,15 +109,6 @@ module "olm" {
   cluster_config_file = module.cluster.config_file_path
   cluster_type = module.cluster.platform.type_code
   cluster_version = module.cluster.platform.version
-}
-module "resource_group" {
-  source = "cloud-native-toolkit/resource-group/ibm"
-  version = "3.3.2"
-
-  ibmcloud_api_key = var.ibmcloud_api_key
-  purge_volumes = var.purge_volumes
-  resource_group_name = var.resource_group_name
-  sync = var.resource_group_sync
 }
 module "sealed-secret-cert" {
   source = "github.com/cloud-native-toolkit/terraform-util-sealed-secret-cert?ref=v1.0.1"
--- 2-standard/200-ibm-openshift-gitops/variables.tf
+++ 2-standard/200-ibm-openshift-gitops/variables.tf
@@ -1,3 +1,107 @@
+variable "gitea_instance_name" {
+  type = string
+  description = "The name for the instance"
+  default = "gitea"
+}
+variable "gitea_username" {
+  type = string
+  description = "The username for the instance"
+  default = "gitea-admin"
+}
+variable "gitea_password" {
+  type = string
+  description = "The password for the instance"
+  default = ""
+}
+variable "config_banner_background_color" {
+  type = string
+  description = "The background color of the top banner. This value can be a named color (e.g. purple, red) or an RGB value (#FF0000)."
+  default = "purple"
+}
+variable "config_banner_text_color" {
+  type = string
+  description = "The text color for the top banner. This value can be a named color (e.g. purple, red) or an RGB value (#FF0000)."
+  default = "white"
+}
+variable "config_banner_text" {
+  type = string
+  description = "The text that will appear in the top banner in the cluster"
+}
+variable "gitops-console-link-job_cluster_ingress_hostname" {
+  type = string
+  description = "Ingress hostname of the IKS cluster."
+  default = ""
+}
+variable "gitops-console-link-job_cluster_type" {
+  type = string
+  description = "The cluster type (openshift or ocp3 or ocp4 or kubernetes)"
+  default = "ocp4"
+}
+variable "gitops-console-link-job_tls_secret_name" {
+  type = string
+  description = "The name of the secret containing the tls certificate values"
+  default = ""
+}
+variable "gitea_namespace_name" {
+  type = string
+  description = "The value that should be used for the namespace"
+  default = "gitea"
+}
+variable "gitea_namespace_ci" {
+  type = bool
+  description = "Flag indicating that this namespace will be used for development (e.g. configmaps and secrets)"
+  default = false
+}
+variable "gitea_namespace_create_operator_group" {
+  type = bool
+  description = "Flag indicating that an operator group should be created in the namespace"
+  default = true
+}
+variable "gitea_namespace_argocd_namespace" {
+  type = string
+  description = "The namespace where argocd has been deployed"
+  default = "openshift-gitops"
+}
+variable "toolkit_namespace_name" {
+  type = string
+  description = "The value that should be used for the namespace"
+  default = "toolkit"
+}
+variable "toolkit_namespace_ci" {
+  type = bool
+  description = "Flag indicating that this namespace will be used for development (e.g. configmaps and secrets)"
+  default = false
+}
+variable "toolkit_namespace_create_operator_group" {
+  type = bool
+  description = "Flag indicating that an operator group should be created in the namespace"
+  default = true
+}
+variable "toolkit_namespace_argocd_namespace" {
+  type = string
+  description = "The namespace where argocd has been deployed"
+  default = "openshift-gitops"
+}
+variable "sealed-secret-cert_cert" {
+  type = string
+  description = "The public key that will be used to encrypt sealed secrets. If not provided, a new one will be generated"
+  default = ""
+}
+variable "sealed-secret-cert_private_key" {
+  type = string
+  description = "The private key that will be used to decrypt sealed secrets. If not provided, a new one will be generated"
+  default = ""
+}
+variable "sealed-secret-cert_cert_file" {
+  type = string
+  description = "The file containing the public key that will be used to encrypt the sealed secrets. If not provided a new public key will be generated"
+  default = ""
+}
+variable "sealed-secret-cert_private_key_file" {
+  type = string
+  description = "The file containin the private key that will be used to encrypt the sealed secrets. If not provided a new private key will be generated"
+  default = ""
+}
 variable "gitops_repo_host" {
   type = string
   description = "The host for the git repository. The git host used can be a GitHub, GitHub Enterprise, Gitlab, Bitbucket, Gitea or Azure DevOps server. If the host is null assumes in-cluster Gitea instance will be used."
@@ -24,26 +128,6 @@ variable "gitops_repo_username" {
   default = ""
 }
 variable "gitops_repo_token" {
-  type = string
-  description = "The personal access token used to access the repository"
-  default = ""
-}
-variable "gitops_repo_gitea_host" {
-  type = string
-  description = "The host for the default gitea repository."
-  default = ""
-}
-variable "gitops_repo_gitea_org" {
-  type = string
-  description = "The org/group for the default gitea repository. If not provided, the value will default to the username org"
-  default = ""
-}
-variable "gitops_repo_gitea_username" {
-  type = string
-  description = "The username of the default gitea repository"
-  default = ""
-}
-variable "gitops_repo_gitea_token" {
   type = string
   description = "The personal access token used to access the repository"
   default = ""
@@ -92,189 +176,71 @@ variable "argocd-bootstrap_create_webhook" {
   description = "Flag indicating that a webhook should be created in the gitops repo to notify argocd of changes"
   default = true
 }
-variable "config_banner_background_color" {
-  type = string
-  description = "The background color of the top banner. This value can be a named color (e.g. purple, red) or an RGB value (#FF0000)."
-  default = "purple"
-}
-variable "config_banner_text_color" {
-  type = string
-  description = "The text color for the top banner. This value can be a named color (e.g. purple, red) or an RGB value (#FF0000)."
-  default = "white"
-}
-variable "config_banner_text" {
-  type = string
-  description = "The text that will appear in the top banner in the cluster"
-}
-variable "gitops-console-link-job_cluster_ingress_hostname" {
-  type = string
-  description = "Ingress hostname of the IKS cluster."
-  default = ""
-}
-variable "gitops-console-link-job_cluster_type" {
-  type = string
-  description = "The cluster type (openshift or ocp3 or ocp4 or kubernetes)"
-  default = "ocp4"
-}
-variable "gitops-console-link-job_tls_secret_name" {
-  type = string
-  description = "The name of the secret containing the tls certificate values"
-  default = ""
-}
-variable "toolkit_namespace_name" {
+variable "gitea_namespace1_name" {
   type = string
   description = "The value that should be used for the namespace"
-  default = "toolkit"
+  default = "gitea"
 }
-variable "toolkit_namespace_ci" {
+variable "gitea_namespace1_ci" {
   type = bool
   description = "Flag indicating that this namespace will be used for development (e.g. configmaps and secrets)"
   default = false
 }
-variable "toolkit_namespace_create_operator_group" {
+variable "gitea_namespace1_create_operator_group" {
   type = bool
   description = "Flag indicating that an operator group should be created in the namespace"
   default = true
 }
-variable "toolkit_namespace_argocd_namespace" {
+variable "gitea_namespace1_argocd_namespace" {
   type = string
   description = "The namespace where argocd has been deployed"
   default = "openshift-gitops"
 }
-variable "ibmcloud_api_key" {
+variable "server_url" {
   type = string
-  description = "The IBM Cloud api key"
+  description = "The url for the OpenShift api"
 }
-variable "resource_group_name" {
+variable "cluster_login_user" {
   type = string
-  description = "The name of the resource group"
-}
-variable "resource_group_sync" {
-  type = string
-  description = "Value used to order the provisioning of the resource group"
+  description = "Username for login"
   default = ""
 }
-variable "purge_volumes" {
+variable "cluster_login_password" {
+  type = string
+  description = "Password for login"
+  default = ""
+}
+variable "cluster_login_token" {
+  type = string
+  description = "Token used for authentication"
+}
+variable "cluster_skip" {
   type = bool
-  description = "Flag indicating that any volumes in the resource group should be automatically destroyed before destroying the resource group. If volumes exist and the flag is false then the destroy will fail."
+  description = "Flag indicating that the cluster login has already been performed"
   default = false
 }
-variable "region" {
+variable "cluster_cluster_version" {
   type = string
-  description = "the value of region"
-}
-variable "cluster_name" {
-  type = string
-  description = "The name of the cluster that will be created within the resource group"
+  description = "[Deprecated] The version of the cluster (passed through to the output)"
   default = ""
 }
-variable "worker_count" {
-  type = number
-  description = "The number of worker nodes that should be provisioned for classic infrastructure"
-  default = 3
-}
-variable "cluster_flavor" {
+variable "cluster_ingress_subdomain" {
   type = string
-  description = "The machine type that will be provisioned for classic infrastructure"
+  description = "[Deprecated] The ingress subdomain of the cluster (passed through to the output)"
   default = ""
 }
-variable "ocp_version" {
+variable "cluster_tls_secret_name" {
   type = string
-  description = "The version of the OpenShift cluster that should be provisioned (format 4.x)"
-  default = "4.10"
-}
-variable "cluster_exists" {
-  type = bool
-  description = "Flag indicating if the cluster already exists (true or false)"
-  default = true
-}
-variable "cluster_disable_public_endpoint" {
-  type = bool
-  description = "Flag indicating that the public endpoint should be disabled"
-  default = false
-}
-variable "name_prefix" {
-  type = string
-  description = "The prefix name for the service. If not provided it will default to the resource group name"
+  description = "[Deprecated] The name of the secret containing the tls certificates for the ingress subdomain (passed through to the output)"
   default = ""
 }
-variable "cluster_ocp_entitlement" {
+variable "cluster_ca_cert" {
   type = string
-  description = "Value that is applied to the entitlements for OCP cluster provisioning"
-  default = "cloud_pak"
-}
-variable "cluster_force_delete_storage" {
-  type = bool
-  description = "Attribute to force the removal of persistent storage associtated with the cluster"
-  default = false
-}
-variable "cluster_tags" {
-  type = string
-  description = "Tags that should be added to the instance"
-  default = "[]"
-}
-variable "cluster_vpc_name" {
-  type = string
-  description = "Name of the VPC instance that will be used"
+  description = "The base64 encoded ca certificate"
   default = ""
 }
-variable "cluster_vpc_subnet_count" {
-  type = number
-  description = "Number of vpc subnets"
-  default = 0
-}
-variable "cluster_vpc_subnets" {
+variable "cluster_ca_cert_file" {
   type = string
-  description = "List of subnets with labels"
-  default = "[]"
-}
-variable "cluster_cos_id" {
-  type = string
-  description = "The crn of the COS instance that will be used with the OCP instance"
-  default = ""
-}
-variable "cluster_kms_enabled" {
-  type = bool
-  description = "Flag indicating that kms encryption should be enabled for this cluster"
-  default = false
-}
-variable "cluster_kms_id" {
-  type = string
-  description = "The crn of the KMS instance that will be used to encrypt the cluster."
-  default = null
-}
-variable "cluster_kms_key_id" {
-  type = string
-  description = "The id of the root key in the KMS instance that will be used to encrypt the cluster."
-  default = null
-}
-variable "cluster_kms_private_endpoint" {
-  type = bool
-  description = "Flag indicating that the private endpoint should be used to connect the KMS system to the cluster."
-  default = true
-}
-variable "cluster_login" {
-  type = bool
-  description = "Flag indicating that after the cluster is provisioned, the module should log into the cluster"
-  default = false
-}
-variable "sealed-secret-cert_cert" {
-  type = string
-  description = "The public key that will be used to encrypt sealed secrets. If not provided, a new one will be generated"
-  default = ""
-}
-variable "sealed-secret-cert_private_key" {
-  type = string
-  description = "The private key that will be used to decrypt sealed secrets. If not provided, a new one will be generated"
-  default = ""
-}
-variable "sealed-secret-cert_cert_file" {
-  type = string
-  description = "The file containing the public key that will be used to encrypt the sealed secrets. If not provided a new public key will be generated"
-  default = ""
-}
-variable "sealed-secret-cert_private_key_file" {
-  type = string
-  description = "The file containin the private key that will be used to encrypt the sealed secrets. If not provided a new private key will be generated"
+  description = "The path to the file that contains the ca certificate"
   default = ""
 }
