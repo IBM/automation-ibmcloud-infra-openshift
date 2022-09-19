0b13ddd2369bf4911e947d7f10bc733e209ec18c
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-09-19 15:01:34 +1000

    Removes VPN timeout
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- 2-standard/110-ibm-vpc-edge-standard/variables.tf
+++ 2-standard/110-ibm-vpc-edge-standard/variables.tf
@@ -409,7 +409,7 @@ variable "ibm-vpn-server_vpn_server_port" {
 variable "ibm-vpn-server_vpn_client_timeout" {
   type = number
   description = "VPN Server Client Time out"
-  default = 3600
+  default = 0
 }
 variable "ibm-vpn-server_enable_split_tunnel" {
   type = bool
