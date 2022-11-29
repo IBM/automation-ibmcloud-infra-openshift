afcf6aef091efe65da15ecac1a2825a9bc93ee7c
rich-ehrhardt <rich_ehrhardt@au1.ibm.com>
2022-11-29 12:18:58 +1100

    Pins provider versions
    Signed-off-by: rich-ehrhardt <rich_ehrhardt@au1.ibm.com>

--- 1-quickstart/210-ibm-portworx-storage/version.tf
+++ 1-quickstart/210-ibm-portworx-storage/version.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     gitops = {
       source = "cloud-native-toolkit/gitops"
+      version = "0.11.1"
     }
 
     ibm = {
       source = "ibm-cloud/ibm"
+      version = "1.47.1"
     }
 
   }
--- 2-standard/200-openshift-gitops/version.tf
+++ 2-standard/200-openshift-gitops/version.tf
@@ -2,10 +2,12 @@ terraform {
   required_providers {
     gitops = {
       source = "cloud-native-toolkit/gitops"
+      version = "0.11.1"
     }
 
     clis = {
       source = "cloud-native-toolkit/clis"
+      version = "0.2.4"
     }
 
   }
